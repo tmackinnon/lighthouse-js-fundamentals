b2494290e987ae798f743f9b1edd01390620cac2
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-16 21:44:49 +0000

    comparing console.log with return

--- hello.js
+++ hello.js
@@ -1,6 +1,15 @@
-const sayHello = function(name) {
+// Uses console.log to print statement 
+const sayHello1 = function(name) {
   console.log("Hello, " + name);
 }
 
-sayHello("Tara");
-sayHello("Adam");
+sayHello1("Adam");
+
+//Uses return in function then console.log outside the function
+const sayHello2 = function(name) {
+  //returns the string "Hello, Tara" to the variable sayHello
+  return "Hello, " + name;
+}
+
+const greeting = sayHello2("Tara");
+console.log(greeting)
