eb2a5b66f16f7708f64e35cf2f1a4d0fc1b74d24
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-16 06:35:37 +0000

    not sure what changes I made here

--- conditionals.js
+++ conditionals.js
@@ -30,3 +30,22 @@ let raining = false
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+//whichschool example
+const whichSchool  = function (age) {
+  if (age < 13) {
+    return "Elementary School"
+  } else if ((age >= 13) && (age <= 18)) {
+    return "Secondary School";
+  } else if (age>18) {
+    return "Lighthouse Labs";
+  }
+}
+
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
