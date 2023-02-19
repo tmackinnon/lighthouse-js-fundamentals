5fb517461cba4e499b686ab96f7f1539eba586a1
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-19 17:30:45 +0000

    Array assignment - ice cream

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//add flavour "root beer" to array
+iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours); 
+
+//print the first flavour in the array 
+console.log(iceCreamFlavours[0]);
+
+//print the last flavour in the array 
+console.log(iceCreamFlavours[5]);
+
+//print the number of elements in the array
+console.log(iceCreamFlavours.length);
+
