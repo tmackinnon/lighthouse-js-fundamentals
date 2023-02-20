682593af6b1f67a2e3fc802d777145f56a3ddb0d
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-20 17:50:05 +0000

    updated index var

--- looping-range.js
+++ looping-range.js
@@ -23,7 +23,7 @@ function lastIndexOf (arr, value) {
   let index;
   for (let i = arr.length - 1; i >= 0; i--) {
     if (value === arr[i]) {
-      index = [i];
+      index = i;
       return index;  
   } 
 } return -1;
