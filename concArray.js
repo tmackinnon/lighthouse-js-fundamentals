ce9f0dddb5d33a57dd5475015e13a7a3ac0ea94f
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-20 17:47:38 +0000

    concatenate two arrays into one assignment

---
+++ concArray.js
@@ -0,0 +1,12 @@
+//concatenate the arrays from both researchers into a single array
+//uses .concat()
+function concat (arrayA, arrayB) {
+  let finalArray = [];
+  finalArray = arrayA.concat(arrayB);
+  return finalArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
