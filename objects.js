2301245dd4fc28be77f1c1d9500cfd2d22d9deb1
Tara MacKinnon <taramackinnon4@gmail.com>
2023-02-20 21:51:52 +0000

    objects driving mayor daisy assignment. Add object to array

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
+// objects trash to treasure assignment 
+/* commented out for next assignment 
 function smartGarbage(trash, bins) {
   if (trash === 'waste') {
     bins.waste += 1;
@@ -9,3 +11,36 @@ function smartGarbage(trash, bins) {
 }
 
 console.log(smartGarbage('waste', { waste: 4, recycling: 2, compost: 5 }));
+*/
+
+//Objects - driving mayor daisy assignment
+// takes in an array of car objects, and the speed of a car as it passes the sensor
+const carPassing = function (cars, speed) {
+  //add new object to cars array
+  let newCar = {
+    time: Date.now(),
+    speed: speed,
+  }
+  cars.push(newCar);
+  return cars;
+}
+
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+console.log(carPassing(cars, speed));
